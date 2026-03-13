248b58c2fb46c9533960171bd463ef13d01448e4
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-03-13 07:13:05 +0300

    playing around with ejs nothing fancy delete them later

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const rateLimit = require("express-rate-limit");
+const path = require("node:path")
 
 
 const authorRouter = require("./routes/authorRouter");
@@ -11,12 +12,26 @@ const birds = require("./routes/birds")
 app.use(cors()); // allow React to call backend
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 
 // API routes
 app.use('/api/birds',birds)
 app.use("/api/authors", authorRouter);
 app.use("/api/books", bookRouter);
 app.use("/api", indexRouter);
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
