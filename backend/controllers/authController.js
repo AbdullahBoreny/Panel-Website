ccb5b750987e35be70e50d2bdd38339b4e8aebfb
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-03-13 07:11:03 +0300

    add a custom error for auth controller, correct post form path in index.html

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,41 +1,40 @@
-// controllers/authorController.js
-
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
 // eslint-disable-next-line no-undef
-const db = require("../db");
+const db = require("../db/db");
 
 async function getAuthorById(req, res) {
   const { authorId } = req.params;
 
-  const author = await db.getAuthorById(Number(authorId));
-  console.log(author);
 
+  const author = await db.getAuthorById(Number(authorId));
+  
+  
   if (!author) {
-    res.status(404).send("Author not found");
-    return;
+    throw new CustomNotFoundError("Author not found");
   }
 
-  res.send(author)
+
+  console.log(author);
+  res.send(author);
 }
+
 async function getAuthors(req, res) {
   const authors = await db.getAuthors();
-  console.log(authors);
   if (!authors) {
     res.status(404).send("Authors not found");
     return;
   }
-  const response = authors
-    .map((author) => `author name: ${author.name}`)
-    .join("\n");
-  console.log(response);
-
-res.send(authors);
+  res.send(authors);
 }
 
 async function postAuthors(req, res) {
   const authors = await db.getAuthors();
   const newAuthor = { id: authors.length + 1, ...req.body };
   authors.push(newAuthor);
+  console.log(authors);
+  
   res.json({ message: "authors added!", author: newAuthor });
 }
+
 
 module.exports = { getAuthorById, getAuthors, postAuthors };
