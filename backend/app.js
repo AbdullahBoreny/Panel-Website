b00bef8374e3fdd28c62c2451d73ffc0faf4e035
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-03-14 03:33:25 +0300

    use port env variable

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,23 @@
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const rateLimit = require("express-rate-limit");
-const path = require("node:path")
+const path = require("node:path");
+const PORT = process.env.PORT || 8080;
 
 
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
-const birds = require("./routes/birds")
+const birds = require("./routes/birds");
 app.use(cors()); // allow React to call backend
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-
-// API routes
-app.use('/api/birds',birds)
+app.use("/api/birds", birds);
 app.use("/api/authors", authorRouter);
 app.use("/api/books", bookRouter);
 app.use("/api", indexRouter);
@@ -33,7 +33,6 @@ app.get("/", (req, res) => {
   res.render("index", { links: links, users: users });
 });
 
-const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
 });
